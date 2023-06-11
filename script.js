8a633841c48c01f4878505cfe6dd7335c2c26a5c
Jury <jury.romblondev@gmail.com>
2023-06-11 16:16:53 +0800

    Update Script

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const secretNum = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNum;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -8,5 +11,14 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.message').textContent =
       'Please Put your guess number';
+  } else if (guess === secretNum) {
+    document.querySelector('.message').textContent =
+      'You Guessed the Correct Number';
+  } else if (guess > secretNum) {
+    document.querySelector('.message').textContent =
+      'The number you guessed is to high. Guess Again';
+  } else {
+    document.querySelector('.message').textContent =
+      'The number you guessed is to low. Guess Again';
   }
 });
