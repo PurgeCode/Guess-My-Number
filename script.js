11ea949c4ba4c93e05964495ebb6ab5d1499ddaa
Jury <jury.romblondev@gmail.com>
2023-06-11 22:03:57 +0800

    Update the Script

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let score = 20;
 let secretNum = Math.trunc(Math.random() * 20) + 1;
 let highScore = 0;
+
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
