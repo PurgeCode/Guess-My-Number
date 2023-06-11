34c59d7a5cf7c7d3ad5e1a004ff3c54ff3f17943
Jury <jury.romblondev@gmail.com>
2023-06-11 15:57:37 +0800

    Update Script

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Please Put your guess number';
+  }
+});
